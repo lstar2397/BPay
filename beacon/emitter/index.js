7d15c2018326e7fc67b4f3a4d77aaf2ddf4fdac7
Starlet <lstar2397@naver.com>
2019-06-09 16:06:00 +0900

    이름 수정

--- beacon/emitter/index.js
+++ beacon/emitter/index.js
@@ -2,7 +2,7 @@ const eddystoneBeacon = require('eddystone-beacon');
 
 let url = 'http://starlet.kr/pay';
 let options = {
-    name: 'My Beacon',
+    name: 'BPay Beacon',
     txPowerLevel: -22,
     tlmCount: 2,
     tlmPeriod: 10
