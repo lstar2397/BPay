bffb5924bed287c5846497b23e3f44296e27c8cf
Starlet <lstar2397@naver.com>
2019-06-09 18:33:45 +0900

    Update app.js

--- beacon/receiver/token-generator/lib/app.js
+++ beacon/receiver/token-generator/lib/app.js
@@ -5,21 +5,13 @@ const mkdirp = require('mkdirp');
 
 const tokenPath = path.join(__dirname, '../bin/token');
 
-function writeFile(path, contents, callback) {
-    mkdirp(path.getDirName(path), err => {
-        if (err) throw callback(err);
-
-        fs.writeFile(path, contents, 'utf-8', callback);
-    });
-}
-
 exports.getToken = function(callback) {
     fs.readFile(tokenPath, 'utf-8', (err, data) => {
         if (err) {
             console.log('토큰을 만들고 있습니다.');
             let token = crypto.randomBytes(12).toString('hex');
 
-            writeFile(tokenPath, token, err => {
+            fs.writeFile(tokenPath, token, 'utf-8', err => {
                 if (err) throw err;
                 console.log('토큰 파일이 저장되었습니다.');
             });
