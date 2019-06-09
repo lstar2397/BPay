86b3b9c22964cf76d8f012d47e7d73eba4692ca2
Starlet <lstar2397@naver.com>
2019-06-09 16:05:52 +0900

    토큰 생성 파일 수정

--- beacon/receiver/token-generator/lib/app.js
+++ beacon/receiver/token-generator/lib/app.js
@@ -7,17 +7,17 @@ const tokenPath = path.join(__dirname, '../bin/token');
 exports.getToken = function(callback) {
     fs.readFile(tokenPath, 'utf-8', (err, data) => {
         if (err) {
-            console.log('Generating token');
-            let token = crypto.randomBytes(24).toString('hex');
+            console.log('토큰을 만들고 있습니다.');
+            let token = crypto.randomBytes(12).toString('hex');
             
             fs.writeFile(tokenPath, token, 'utf-8', err => {
                 if (err) throw err;
-                console.log('Token file has been saved!');
+                console.log('토큰 파일이 저장되었습니다.');
             });
             
             callback(token);
         } else {
-            console.log('Using token file');
+            console.log('토큰 파일을 이용합니다.');
             callback(data);
         }
     });
