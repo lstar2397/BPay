0a35892ea16eb586b6ffd521361bd69cfac02d85
Starlet <lstar2397@naver.com>
2019-06-09 18:25:40 +0900

    리시버 업데이트

--- beacon/receiver/token-generator/lib/app.js
+++ beacon/receiver/token-generator/lib/app.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
+const mkdirp = require('mkdirp');
 
 const tokenPath = path.join(__dirname, '../bin/token');
+
+function writeFile(path, contents, callback) {
+    mkdirp(path.getDirName(path), err => {
+        if (err) throw callback(err);
+
+        fs.writeFile(path, contents, 'utf-8', callback);
+    });
+}
 
 exports.getToken = function(callback) {
     fs.readFile(tokenPath, 'utf-8', (err, data) => {
         if (err) {
             console.log('토큰을 만들고 있습니다.');
             let token = crypto.randomBytes(12).toString('hex');
-            
-            fs.writeFile(tokenPath, token, 'utf-8', err => {
+
+            writeFile(tokenPath, token, err => {
                 if (err) throw err;
                 console.log('토큰 파일이 저장되었습니다.');
             });
             
             callback(token);
         } else {
-            console.log('토큰 파일을 이용합니다.');
+            console.log('저장된 토큰 파일을 이용합니다.');
             callback(data);
         }
     });
