81dade06449389f8c1bbe7d5aa20d3c07fa8b914
Starlet <lstar2397@naver.com>
2019-06-09 18:55:35 +0900

    웹 업데이트

--- web/index.js
+++ web/index.js
@@ -3,15 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 80;
+
 const indexPage = require('./routes');
 const payPage = require('./routes/pay');
 const amountPage = require('./routes/amount');
+const historyPage = require('./routes/history');
 
 app.use(bodyParser.json());
 
 app.use('/', indexPage);
 app.use('/pay', payPage);
 app.use('/amount', amountPage);
+app.use('/history', historyPage);
 
 app.listen(port, () => {
     console.log('Server has started on port ' + port);
